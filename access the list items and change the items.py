94cac063ec70bb30f34be66dfd593763c9fe3309
nitheshkotrike <nitheshkotrike1@gmail.com>
2024-02-10 18:12:34 +0530

    added for loop and printing the length of each fruit from the fruits_market1 list

--- access the list items and change the items.py
+++ access the list items and change the items.py
@@ -7,3 +7,17 @@ print(fruits_market2[2])
 fruits_market2[2] = 'watermelon'
 
 print(fruits_market2)
+
+for fruit in fruits_market1:
+    a = len(fruit)
+    b = fruit
+    print(b,'=',a)
+
+
+
+
+# a = len(fruit)
+#     b = fruit
+
+
+# print(a, '=' ,b)
