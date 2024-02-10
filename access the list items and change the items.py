cab2f188360a77daf0121a8f491158c140b2055d
nitheshkotrike <nitheshkotrike1@gmail.com>
2024-02-10 19:20:51 +0530

    appending fruits_market2 list to fruits_market3 using for loop and created 1 branch from vs (branchvs_python_lists)

--- access the list items and change the items.py
+++ access the list items and change the items.py
@@ -2,6 +2,8 @@ fruits_market1 = ['apple','banana','carret']
 
 fruits_market2 = ['custed apple','pineapple','orange']
 
+fruits_market3 = []
+
 print(fruits_market2[2])
 
 fruits_market2[2] = 'watermelon'
@@ -12,6 +14,12 @@ for fruit in fruits_market1:
     a = len(fruit)
     b = fruit
     print(b,'=',a)
+
+for fruits2 in fruits_market2:
+    a = fruits2
+    fruits_market3.append(a)
+    
+print(fruits_market3)   
 
 
 
