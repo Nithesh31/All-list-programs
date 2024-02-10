4437afbdc0622f885a926a694b49375c9b0f8bab
nitheshkotrike <nitheshkotrike1@gmail.com>
2024-02-10 15:39:20 +0530

    adding access the list python file in this repo

---
+++ access the list items and change the items.py
@@ -0,0 +1,9 @@
+fruits_market1 = ['apple','banana','carret']
+
+fruits_market2 = ['custed apple','pineapple','orange']
+
+print(fruits_market2[2])
+
+fruits_market2[2] = 'watermelon'
+
+print(fruits_market2)
